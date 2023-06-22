7ce1ad103ad1ebdf272dbc91131b0fb2471d5b79
Jiahui <122433081+Jh5214@users.noreply.github.com>
2023-06-22 22:21:58 +0800

    Update FileUploadForm.js

--- FileUploadForm.js
+++ FileUploadForm.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
-import { useNavigate } from 'react-router-dom';
-import * as XLSX from "xlsx";
+import { OutTable, ExcelRenderer } from "react-excel-renderer";
 import Hero from "../components/Hero.js";
 
 const supabase = createClient("https://jedendeblvtzvmbtgmsv.supabase.co",
@@ -9,30 +8,26 @@ const supabase = createClient("https://jedendeblvtzvmbtgmsv.supabase.co",
 ); // Replace with your Supabase credentials
 
 function FileUploadForm() {
-  const navigate = useNavigate();
+  const [header, setHeader] = useState([]);
+  const [cols, setCols] = useState([]);
+  const [state, setState] = useState(false);
   const [selectedFile, setSelectedFile] = useState(null);
   const [uploadStatus, setUploadStatus] = useState(null);
-  const [data, setData] = useState([]);
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     setSelectedFile(file);
-  };
+    ExcelRenderer(file, (err, response) => {
+      if (err) {
+        console.log(err);
+      } else {
+        setHeader(response.rows[0]);
+        setCols(response.rows);
+      }
+    });
+  }
 
-  const handleUpload = async (event) => {
-    if (event && event.target && event.target.files && event.target.files.length > 0) {
-      const reader = new FileReader();
-      reader.readAsBinaryString(event.target.files[0]);
-      reader.onload = (e) => {
-        const data = e.target.result;
-        const workbook = XLSX.read(data, { type: "binary" });
-        const sheetName = workbook.SheetNames[0];
-        const sheet = workbook.Sheets[sheetName];
-        const parsedData = XLSX.utils.sheet_to_json(sheet);
-        setData(parsedData);
-      };
-    }
-
+  const handleUpload = async () => {
     if (selectedFile) {
       // Upload the file to Supabase Storage directly into the "excel" bucket
       const { data, error } = await supabase.storage
@@ -51,10 +46,6 @@ function FileUploadForm() {
     }
   };
 
-  const handleNext = () => {
-    navigate('/excel-viewer');
-  };
-
   return (
     <div>
       <Hero
@@ -63,120 +54,47 @@ function FileUploadForm() {
         url="/"
         next="hide"
       />
-      <h1>File Upload Form</h1>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload}>Upload</button>
-      {uploadStatus && <p>{uploadStatus}</p>}
-      <button onClick={handleNext}>Next</button>
-      <input type="file" accept=".xlsx, .xls" onChange={(event) => handleUpload(event)} style={{ display: "block", marginleft: "auto", marginright: "auto" }} />
-      {data.length > 0 && (
-        <table className="table">
-          <thead>
-            <tr>
-              {Object.keys(data[0]).map((key) => (
-                <th key={key}>{key}</th>
+      <div style={{ margin: "10px auto"  }}>
+        <h1>File Upload Form</h1>
+        <input type="file" onChange={handleFileChange} ></input>
+      </div>
+      <br />
+      <table
+        style={{
+          borderCollapse: "collapse",
+          margin: "10px auto",
+          border: "1px solid black",
+          width: "100%"
+        }}
+      >
+
+          <tr>
+            {header.map((h, i) => (
+              <th
+                style={{
+                  border: "1px solid black"
+                }}
+                key={i}
+              >
+                {h}
+              </th>
+            ))}
+          </tr>
+          {cols.slice(1).map((col, i) => (
+            <tr key={i}>
+              {col.map((c, i) => (
+                <td style={{ border: "1px solid black" }} key={i}>
+                  {c}
+                </td>
               ))}
             </tr>
-          </thead>
-          <tbody>
-            {data.map((row, index) => (
-              <tr key={index}>
-                {Object.values(row).map((value, index) => (
-                  <td key={index}>{value}</td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </div>
-    
-    /*<div>
-      <br>
-        </br>
-        <br>
-        </br>
-        <br>
-        </br>
-        <br>
-        </br>
-        <br>
-        </br>
-        <br>
-        </br>
-        <br>
-        </br>
-        <br>
-        </br>
-      <h1>File Upload Form</h1>
-      <input type="file" onChange={handleFileChange} />
+          ))}
+      </table>
+      
       <button onClick={handleUpload}>Upload</button>
       {uploadStatus && <p>{uploadStatus}</p>}
-      <button onClick={handleNext}>Next</button>
-    </div>*/
+    </div>
   );
 }
 
 export default FileUploadForm;
-
-
-/*import { useState } from 'react';
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-    "https://jedendeblvtzvmbtgmsv.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImplZGVuZGVibHZ0enZtYnRnbXN2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4NDgyNjIyOSwiZXhwIjoyMDAwNDAyMjI5fQ.B22JM-wZyJlj2Brtx7keClIgkFE_Y_-4SXeQnAp6HGE"
-  );
-
-function FileUploadForm() {
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [uploadStatus, setUploadStatus] = useState(null);
-
-  const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    setSelectedFile(file);
-  };
-
-  const handleUpload = async () => {
-    if (selectedFile) {
-      // Upload the file to Supabase Storage directly into the "excel" bucket
-      const { data, error } = await supabase.storage
-        .from('excel') // Replace with your specific bucket name
-        .upload(selectedFile.name, selectedFile, {cacheControl: '3600', upsert: false});
-
-      if (error) {
-        console.error('Error uploading file:', error);
-        setUploadStatus('Upload failed: ' + error.message);
-      } else {
-        console.log('File uploaded successfully:', data.Key);
-        setUploadStatus('Upload successful');
-      }
-    }
-  };
-
-  return (
-    <div>
-        <br>
-        </br>
-        <br>
-        </br>
-        <br>
-        </br>
-        <br>
-        </br>
-        <br>
-        </br>
-        <br>
-        </br>
-        <br>
-        </br>
-        <br>
-        </br>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload}>Upload</button>
-      {uploadStatus && <p>{uploadStatus}</p>}
-    </div>
-  );
-}
-
-export default FileUploadForm;*/
