d787bd20c8a816a98a369117672522dbb94a6053
Q8yi <122433084+Q8yi@users.noreply.github.com>
2023-06-24 23:05:28 +0800

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
-  "https://jedendeblvtzvmbtgmsv.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImplZGVuZGVibHZ0enZtYnRnbXN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ4MjYyMjksImV4cCI6MjAwMDQwMjIyOX0.lejG0c-0yaUsfd0optvquxA9shNvvFjYHOwzXPX5BS4"
-);
