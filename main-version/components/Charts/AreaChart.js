3031ed3072e61ed1ea7b321edb5e19e45c58c1d0
Q8yi <122433084+Q8yi@users.noreply.github.com>
2023-06-25 21:05:05 +0800

    Add files via upload

---
+++ main-version/components/Charts/AreaChart.js
@@ -0,0 +1,73 @@
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Filler,
+    Legend, }
+    from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { tempData } from '../tempData';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Filler,
+    Legend
+);
+
+const options = {
+    responsive : true,
+    plugins : {
+        legend : {
+            position: 'top',
+        },
+        title : {
+            display: true,
+            text: 'area chart',
+        },
+    },
+};
+
+const labels = tempData.map((val) => val.year);
+
+const data = {
+    labels,
+    datasets: [
+    {
+        label: 'first',
+        data : tempData.map((val) => val.value),
+        fill : true,
+        borderColor: 'rgb(255,99,152)',
+        backgroundColor: 'rgba(255,99,152,0.5)',
+    },
+    {
+        label: 'second',
+        data: tempData.map((val) => val.value - 2),
+        fill : true,
+        borderColor: 'rgb(75,192,192)',
+        backgroundColor: 'rgba(75,192,192,0.5)',
+    },
+    {
+        label: 'third',
+        data: tempData.map((val) => val.value + 5 + 5),
+        fill : true,
+        borderColor: 'rgb(53,162,235)',
+        backgroundColor: 'rgba(53,162,235,0.5)',
+    }
+],};
+
+export default function AreaChart( {chartData} ) {
+    return (
+        <>
+        <h2 style = {{textAlign: "center"}}>Area Chart</h2>
+        <Line options = {options} data = {data} />
+        </>);
+}
---
+++ main-version/components/Instru.js
@@ -0,0 +1,36 @@
+import "./instruStyles.css";
+import { InstruList } from "./InstruList.js";
+import { Button } from "@mui/material";
+
+export default function Instru() {
+    return (
+        <div className="head">
+            
+            <h1>
+                To get Started
+            </h1>
+            <p> instructions on how to start</p>
+            <a href = "/">
+            <Button variant = "contained">Start Creation</Button>
+            </a>
+            {InstruList.map((instru,index) => {
+            return (
+                <div className = "steps">
+                <div className="steps-text">
+                            <h2>
+                            {instru.heading}
+                            </h2>
+                            <p>
+                                {instru.text}
+                            </p>
+                        </div>
+                            <div className = "image">
+                            <img alt= {instru.heading} src = {instru.img}/>
+                            <img alt = {instru.heading} src = {instru.img}/>
+                            </div>
+                        
+                </div>
+            );})}
+        </div>
+    );
+}
