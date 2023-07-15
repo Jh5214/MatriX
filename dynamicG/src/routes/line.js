2c051522d1797047101551154790c47e7942a377
Jiahui <122433081+Jh5214@users.noreply.github.com>
2023-07-15 22:26:29 +0800

    Update line.js

--- dynamicG/src/routes/line.js
+++ dynamicG/src/routes/line.js
@@ -1,26 +1,35 @@
 //How to Render and Display Excel Spreadsheets on a Webpage with React JS
 
-import React, { useContext , useState } from "react";
+import React, { useContext, useState, useRef } from "react";
 import Hero from "../components/Hero.js";
-import { Line } from 'react-chartjs-2';
-import { TransferDataContext } from './context';
-import {Chart as ChartJS} from 'chart.js/auto';
+import { Line } from "react-chartjs-2";
+import { TransferDataContext } from "./context";
+import { saveAs } from "file-saver";
+import html2canvas from "html2canvas";
 
-
-export default function LinePage(props) { 
-
+export default function LinePage(props) {
   const { labels } = useContext(TransferDataContext);
   const { datas } = useContext(TransferDataContext);
   const { title } = useContext(TransferDataContext);
 
-  const blank = {
+  const chartRef = useRef(null);
+  const [chartData, setChartData] = useState({
     labels: labels ? labels : [],
-    datasets: [{
-      label: title ? title : "heading",
-      data: datas ? datas : []
-    }
-  ]
-  }
+    datasets: [
+      {
+        label: title ? title : "heading",
+        data: datas ? datas : [],
+      },
+    ],
+  });
+
+  const handleDownload = () => {
+    html2canvas(chartRef.current).then((canvas) => {
+      canvas.toBlob((blob) => {
+        saveAs(blob, "chart.png");
+      });
+    });
+  };
 
   return (
     <div>
@@ -31,10 +40,18 @@ export default function LinePage(props) {
         url="/"
         next="hide"
       />
-      <div style = {{height:"70vh",position:"relative", marginBottom:"1%", padding:"1%"}}>
-      <Line data = {blank} 
-              options={{ maintainAspectRatio: false}} />
+      <div
+        style={{
+          height: "70vh",
+          position: "relative",
+          marginBottom: "1%",
+          padding: "1%",
+        }}
+        ref={chartRef}
+      >
+        <Line data={chartData} options={{ maintainAspectRatio: false }} />
       </div>
+      <button onClick={handleDownload}>Download Chart</button>
     </div>
   );
 }
