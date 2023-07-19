4817139e5b69e39f3840bd795459535b1eedf990
Q8yi <122433084+Q8yi@users.noreply.github.com>
2023-07-19 23:19:42 +0800

    Update App.js

--- dynamicG/src/App.js
+++ dynamicG/src/App.js
@@ -52,9 +52,9 @@ export default function App() {
         <Route path="/graphs/bubblebarchart" element={<BarChart />} />
         <Route path="/graphs/radarchart" element={<RadarGraph />} />
       </Routes>
-      </TransferDataProvider>
       <Navbar />
       <Footer/>
+      </TransferDataProvider>
     </div>
     </div>
   );
