f79574306e69b14be789a519d8e1823f22ad1966
Q8yi <122433084+Q8yi@users.noreply.github.com>
2023-07-23 20:46:31 +0800

    Update index.js

--- dynamicG/src/index.js
+++ dynamicG/src/index.js
@@ -4,12 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+import {SessionContextProvider} from "@supabase/auth-helpers-react";
+import { createClient } from "@supabase/supabase-js";
 
+const supabase = createClient(
+  "https://jedendeblvtzvmbtgmsv.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImplZGVuZGVibHZ0enZtYnRnbXN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ4MjYyMjksImV4cCI6MjAwMDQwMjIyOX0.lejG0c-0yaUsfd0optvquxA9shNvvFjYHOwzXPX5BS4"
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+  <SessionContextProvider supabaseClient = {supabase}>
     <App />
+  </SessionContextProvider>
   </BrowserRouter>
 );
 
@@ -17,3 +25,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
