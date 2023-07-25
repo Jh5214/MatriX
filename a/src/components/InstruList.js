447bc032164602c38d3228834d79ee1cc37afe01
Q8yi <122433084+Q8yi@users.noreply.github.com>
2023-07-25 22:20:56 +0800

    Update InstruList.js

--- a/src/components/InstruList.js
+++ a/src/components/InstruList.js
@@ -1,33 +1,33 @@
 export const InstruList = [
     {
-      img: require("../images/bar.png"),
+      img: require("../images/login.png"),
+      img1: require("../images/login1.png"),
       heading: "Account creation",
       text: "It helps in saving works and brings convenience"
     },
   
     {
-        img: require("../images/bar.png"),
+        img: require("../images/uploading.png"),
+        img1: require("../images/cleaning.png"),
         heading: "Uploading and entering data",
         text: "useful for cleaning data to prevent any misleading data visualisation"
     },
     {
-        img: require("../images/bar.png"),
+        img: require("../images/graph.png"),
+        img1: require("../images/bar.png"),
         heading: "Choosing/ Customizing charts",
         text: "Selecting charts that fits the concept of the data set. This will ease the process to analyse output"
         
     },
     {
-        img: require("../images/bar.png"),
-        heading: "Outlier Removal        ",
-        text: "Enhance accuracy in comparison"
-    },
-    {
-        img: require("../images/bar.png"),
-        heading: "Prediction of future data        ",
+        img: require("../images/future.png"),
+        img1: require("../images/future1.png"),
+        heading: "Prediction of future data",
         text: "Aiding business decision"
     },
     {
-        img: require("../images/bar.png"),
+        img: require("../images/download.png"),
+        img1: require("../images/download1.png"),
         heading: "Download",
         text: "Convenient for offline viewing of data and graphs        "
     }
